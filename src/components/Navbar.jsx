36374d13d23abe82d65b64e65bb34a33238f401f
abhiputgov <abhipg@outlook.com>
2023-12-04 17:16:05 +0530

    refining the pages and adding product category and product details page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,48 @@ import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  height: 60px;
+`;
+
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
-  justify-content: Space-between;
-  position: relative;
+  align-items: center;
+  justify-content: space-between;
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
 `;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
 const Center = styled.div`
   flex: 1;
-  display: flex;
   text-align: center;
-  align-items: center;
-  justify-content: center;
-  right: 0;
-  left: 0;
-  position: absolute;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  font-size: 20;
 `;
 const Right = styled.div`
   flex: 1;
@@ -31,33 +52,13 @@ const Right = styled.div`
   align-items: center;
   justify-content: flex-end;
 `;
-const Language = styled.span`
-  margin: 5px;
+
+const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
-`;
-const SearchContainer = styled.div`
-  border: 0.5px solid lightgray;
-  display: flex;
-  align-items: center;
-  border: 1px solid lightgray;
-  margin-left: 25px;
-  padding: 3px;
-  height: 30%;
-`;
-const Input = styled.input`
-  border: none;
-  height: 100%;
-  cursor: pointer;
-`;
-const Logo = styled.div`
-  font-weight: bold;
-`;
-const MenuItem = styled.div`
-  font-size: 19px;
-  cursor: pointer;
   margin-left: 25px;
 `;
+
 const Navbar = () => {
   return (
     <Container>
@@ -65,16 +66,16 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
-            <Search style={{ color: 'gray', fontSize: '16px' }} />
+            <Input placeholder="Search" />
+            <Search style={{ color: 'gray', fontSize: 16 }} />
           </SearchContainer>
         </Left>
         <Center>
           <Logo>P G.</Logo>
         </Center>
         <Right>
-          <MenuItem>Register</MenuItem>
-          <MenuItem>Sign In</MenuItem>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
           <MenuItem>
             <Badge badgeContent={4} color="primary">
               <ShoppingCartOutlined />
