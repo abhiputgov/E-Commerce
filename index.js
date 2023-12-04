93db2fe6eb9328c4f1f34a2a027d86de7d80f56d
abhiputgov <abhipg@outlook.com>
2023-12-04 20:25:48 +0530

    first test and refactor of the express code

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ require('dotenv').config();
 const app = require('express')();
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('DB connection established.....');
+app.get('/api/test', () => {
+  console.log('first test is successful');
+});
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI + '?dbname=e-commerce');
     app.listen(process.env.SERVER_PORT, () => {
-      console.log(
-        `Backend server is listening on port ${process.env.SERVER_PORT}......`,
-      );
+      console.log(`Server listening on port ${process.env.SERVER_PORT}`);
     });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
