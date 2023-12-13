c2bacbe3909ad92e56ee224ff4e52d14ac19e813
abhiputgov <abhipg@outlook.com>
2023-12-13 21:30:38 +0530

    adding all pages in UI

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,17 +1,21 @@
-import Product from './pages/Product';
-import Home from './pages/ Home';
-import ProductList from './pages/ProductList';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
+import Product from "./pages/Product";
+import Home from "./pages/ Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
-  //return <Home />;
-  //return <ProductList />;
-  //return <Product />;
-  //return <Register />;
-  //return <Login />;
-  return <Cart />;
+  return (
+    <Container>
+      <Register />
+      <Login />
+      <Home />
+      <ProductList />
+      <Product />
+      <Cart />
+    </Container>
+  );
 };
 
 export default App;
