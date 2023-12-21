7677aa307220aacb30e0f003c6ff9531a3151cd9
abhiputgov <abhipg@outlook.com>
2023-12-21 13:29:40 +0530

    small correction

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,11 +1,13 @@
-import Product from "./pages/Product";
-import Home from "./pages/ Home";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Cart from "./pages/Cart";
+import Product from './pages/Product';
+import Home from './pages/ Home';
+import ProductList from './pages/ProductList';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
+import styled from 'styled-components';
 
 const App = () => {
+  const Container = styled.div``;
   return (
     <Container>
       <Register />
