3f98d4e81ac1a58e46b0cf0154e56ad17640dd79
abhiputgov <abhipg@outlook.com>
2023-12-21 16:21:12 +0530

    adding router to FE

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -6,20 +6,26 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import styled from 'styled-components';
 import Payments from './pages/Payments';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 
 const App = () => {
-  const Container = styled.div``;
-  return (
-    <Container>
-      <Register />
-      <Login />
-      <Home />
-      <ProductList />
-      <Product />
-      <Cart />
-      <Payments />
-    </Container>
-  );
+  const loggedIn = true;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Outlet />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/products', element: <ProductList /> },
+        { path: '/products/:category', element: <ProductList /> },
+        { path: '/products/product/:id', element: <Product /> },
+        { path: '/cart', element: <Cart /> },
+        { path: '/login', element: <Login /> },
+        { path: '/register', element: <Register /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
